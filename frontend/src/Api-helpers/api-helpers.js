fe2c664b89e921b193d4bd15ded86d188305fdf2
hS71Ru <harsh.sara45@gmail.com>
2023-05-16 00:02:00 +0530

    header & api-helper

---
+++ frontend/src/Api-helpers/api-helpers.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const getAllMovie = async() => {
+    const res=await axios.get("http://localhost:3500/movie")
+    .catch((err)=>
+    console.log(err));
+    
+    if(res.status!==200){
+        return console.log("No Data")
+    }
+    const data=await res.data;
+    return data;
+}
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,43 @@
-import React from 'react'
-import {AppBar,Box, Toolbar} from '@mui/material'
-import TheaterComedySharpIcon from '@mui/icons-material/TheaterComedySharp';
+import React,{useEffect, useState} from 'react'
+import {AppBar, Autocomplete,Box,Tab,Tabs,TextField,Toolbar} from '@mui/material'
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import { getAllMovie } from '../Api-helpers/api-helpers';
 
-const Header=()=>{
-    return (
-    <AppBar>
-        <Toolbar>
-            <Box width={'20%'}>
-            <TheaterComedySharpIcon />
-            </Box>
-        </Toolbar>
-        
-    </AppBar>
+
+
+const dummyArray=["pathan","selfie","gulmohar","munna"]
+const Header = () =>{
+    const [value , setValue]=useState(0);
+    useEffect(()=>{
+        getAllMovie
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err));
+    }, []);
+    return(
+        <AppBar sx={{bgcolor:"#2b2d42"}}>
+            <Toolbar>
+                <Box width={'20%'}>
+                    <TheaterComedyIcon />
+                </Box>
+                <Box width={'30%'} margin={'auto'}>
+                    <Autocomplete
+                        freeSolo
+                        options={dummyArray.map((option)=>option)}
+                        renderInput={(params)=>(  <TextField sx={{input:{color:"white"}}} varient='standard' {...params} placeholder="Search Multiple Movies Across"/>)}
+                    />
+                </Box>
+                <Box display={'flex'}>
+                    <Tabs textColor='white' indicatorColor='secondry' value={value}>
+                    onChenge={(e,val)=>setValue(val)}
+                        <Tab label="Movies"/>
+                        <Tab label="Auth"/>
+                        <Tab label="Admin"/>
+                    </Tabs>
+
+                </Box>
+            </Toolbar>
+
+        </AppBar>
     )
 
 }
